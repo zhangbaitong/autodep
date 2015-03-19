5def33dd76a84e5e85941612e408ff9481716198
tomzhaogy <tomzhao@localhost>
2015-03-19 10:43:48 +0800

    有关仓库动作实现

---
+++ src/api/common/types.go
@@ -0,0 +1,15 @@
+package common
+
+type ACTION_V1 struct {
+	Action string
+                Result string
+}
+
+type RequestData struct
+{
+        Version         string
+        ServerIP        string
+        Port             int
+        Method         string
+        Params         string
+}
