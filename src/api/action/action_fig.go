9a3d7df4a4d78bfc770332942dc2dc720278b0e5
tomzhaogy <tomzhao@localhost>
2015-03-19 10:45:22 +0800

    有关各个模块动作实现，分成四个文件

---
+++ src/api/action/action_fig.go
@@ -0,0 +1,16 @@
+package action
+import (
+    "github.com/codeskyblue/go-sh"
+    "fmt"
+    "api/common"
+)
+
+func FigCreate(request *common.RequestData) string {
+    session := sh.NewSession()
+    session.ShowCMD = true
+    err := session.Call("ssh",request.ServerIP, "touch ","tt.aa")
+    if err != nil {
+            fmt.Println("exec remote shell faild error:", err)
+    }
+    return "go-sh"
+}
