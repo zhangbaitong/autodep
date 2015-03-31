ab8362b298d82a18649297921ade8954aaf48a7f
show168 <1220642745@qq.com>
2015-03-31 16:43:55 +0800

    增加服务器查询功能

--- src/api/handlers/v1.go
+++ src/api/handlers/v1.go
@@ -109,6 +109,10 @@ func API_V1(w http.ResponseWriter, r *http.Request) {
 		{
 			ret = action.RegisterMachine(request)
 		}
+	case "machine/search":
+		{
+			ret = action.SearchMachine(request)
+		}
 	}
 	v1 := common.Response{Method: strMethod, Code: 0, Messgae: "ok", Data: ret}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
